db5a8caefd0440f0ad7ed156f3f404fb2927e1c5
ksbm009 <kevinstephenbiju2003@gmail.com>
2023-06-02 20:49:08 +0530

    small update trial part 2

--- Simple.java
+++ Simple.java
@@ -1,18 +1,18 @@
-import javax.swing.*;  
+import javax.swing.*;
 public class Simple extends JFrame{
-    //Inheriting JFrame  
-    JFrame f;  
+    //Inheriting JFrame
+    JFrame f;
     Simple(){
-        //Create button  
+        //Create button
         JButton b=new JButton("Login");  
-        b.setBounds(130,100,100, 40);  
+        b.setBounds(130,100,100, 40);
         //Adding button on frame
         add(b);
         setSize(400,500);  
-        setLayout(null);  
-        setVisible(true);  
+        setLayout(null);
+        setVisible(true);
     }  
-    public static void main(String[] args) {  
+    public static void main(String[] args){
         new Simple();
     }
-}  
+}
