1197004754b43a1249527ec659c6d619e90b5967
ksbm009 <kevinstephenbiju2003@gmail.com>
2023-06-02 19:05:44 +0530

    swing trial part 2

--- Simple.java
+++ Simple.java
@@ -1,23 +1,18 @@
-import javax.swing.*;
-public class Simple{
-    JFrame f;
+import javax.swing.*;  
+public class Simple extends JFrame{
+    //Inheriting JFrame  
+    JFrame f;  
     Simple(){
-        //Creating a instance of JFrame
-        f = new JFrame();
-
-        //Creating a instance of JButton
-        JButton b = new JButton("Login");
-        b.setBounds(130, 100, 100, 40);
-        //Adding button in JFrame
-        f.add(b);
-          
-        f.setSize(400,500);  
-        //Using no layout managers
-        f.setLayout(null);
-        //Making the frame visible
-        f.setVisible(true);  
-    }
-    public static void main(String[] args){  
-        new Simple();
+        //Create button  
+        JButton b=new JButton("Login");  
+        b.setBounds(130,100,100, 40);  
+        //Adding button on frame
+        add(b);
+        setSize(400,500);  
+        setLayout(null);  
+        setVisible(true);  
     }  
-}
+    public static void main(String[] args) {  
+        new Simple();
+    }
+}  
