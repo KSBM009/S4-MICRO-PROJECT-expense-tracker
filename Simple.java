b13bb1a25a3f837c9e214eabc86b1a905bef912b
ksbm009 <kevinstephenbiju2003@gmail.com>
2023-06-02 15:51:23 +0530

    swing trial part1

---
+++ Simple.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+public class Simple{
+    JFrame f;
+    Simple(){
+        //Creating a instance of JFrame
+        f = new JFrame();
+
+        //Creating a instance of JButton
+        JButton b = new JButton("Click");
+        b.setBounds(130, 100, 100, 40);
+        //Adding button in JFrame
+        f.add(b);
+          
+        f.setSize(400,500);  
+        //Using no layout managers
+        f.setLayout(null);
+        //Making the frame visible
+        f.setVisible(true);  
+    }
+    public static void main(String[] args){  
+        new Simple();
+    }  
+}
