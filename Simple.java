cc6cf0389d7baa351b36f89c269ad429736f30de
ksbm009 <kevinstephenbiju2003@gmail.com>
2023-06-04 00:14:32 +0530

    swing trial part 3 modified

--- Simple.java
+++ Simple.java
@@ -5,7 +5,7 @@ class Simple{
 
 		Label l1 = new Label("Select known Languages");
 
-		l1.setBounds(100, 50, 120, 80);
+		l1.setBounds(100, 50, 200, 80);
 		f.add(l1);
 
 		Checkbox c2 = new Checkbox("Hindi");
@@ -14,7 +14,7 @@ class Simple{
 		Checkbox c3 = new Checkbox("English");
 		c3.setBounds(100, 200, 80, 50);
 		f.add(c3);
-		Checkbox c4 = new Checkbox("marathi");
+		Checkbox c4 = new Checkbox("Marathi");
 		c4.setBounds(100, 250, 80, 50);
 		f.add(c4);
 
