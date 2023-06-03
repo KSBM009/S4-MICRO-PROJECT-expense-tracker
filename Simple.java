74180e6eecdb9165249366cef24388f78e5c2d64
ksbm009 <kevinstephenbiju2003@gmail.com>
2023-06-04 00:07:05 +0530

    swing trial part 3

--- Simple.java
+++ Simple.java
@@ -1,18 +1,29 @@
-import javax.swing.*;
-public class Simple extends JFrame{
-    //Inheriting JFrame
-    JFrame f;
-    Simple(){
-        //Create button
-        JButton b=new JButton("Login");
-        b.setBounds(130,100,100, 40);
-        //Adding button on frame
-        add(b);
-        setSize(400,500);
-        setLayout(null);
-        setVisible(true);
-    }
-    public static void main(String[] args){
-        new Simple();
-    }
+import java.awt.*;
+class Simple{
+	Simple(){
+		Frame f = new Frame();
+
+		Label l1 = new Label("Select known Languages");
+
+		l1.setBounds(100, 50, 120, 80);
+		f.add(l1);
+
+		Checkbox c2 = new Checkbox("Hindi");
+		c2.setBounds(100, 150, 50, 50);
+		f.add(c2);
+		Checkbox c3 = new Checkbox("English");
+		c3.setBounds(100, 200, 80, 50);
+		f.add(c3);
+		Checkbox c4 = new Checkbox("marathi");
+		c4.setBounds(100, 250, 80, 50);
+		f.add(c4);
+
+		f.setSize(500, 500);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+	
+	public static void main(String ar[]){
+		new Simple();
+	}
 }
