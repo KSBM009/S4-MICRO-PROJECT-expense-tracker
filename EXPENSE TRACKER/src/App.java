b60b42cd0e1839b454721eaf1c5cc7c6b78808be
ksbm009 <kevinstephenbiju2003@gmail.com>
2023-06-09 21:36:58 +0530

    loginForm #1

--- EXPENSE TRACKER/src/App.java
+++ EXPENSE TRACKER/src/App.java
@@ -1,35 +1,81 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
 
-public class App{
-    public static void main(String[] args){
-        Connection conn = null;
+class LoginForm extends JFrame {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
 
-        try{
-            // Load the MySQL Connector/J driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
+    private final String DB_URL = "jdbc:mysql://localhost:3306/your_database_name";
+    private final String DB_USER = "your_username";
+    private final String DB_PASSWORD = "your_password";
 
-            // Establish a connection to the MySQL database
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_s4_mini_project", "root", "4112003");
+    public LoginForm() {
+        setTitle("Login Form");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            // Connection successful
-            System.out.println("Connected to the database!");
+        JPanel panel = new JPanel(new GridLayout(3, 2));
 
-            // Perform database operations...
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameField = new JTextField(20);
 
-        } catch (SQLException | ClassNotFoundException e) {
-            // Connection error
-            e.printStackTrace();
-        } finally {
-            // Close the connection
-            if (conn != null) {
-                try{
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordField = new JPasswordField(20);
+
+        loginButton = new JButton("Login");
+
+        panel.add(usernameLabel);
+        panel.add(usernameField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(loginButton);
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                char[] password = passwordField.getPassword();
+
+                if (authenticate(username, password)) {
+                    JOptionPane.showMessageDialog(null, "Login Successful");
+                    // Add code for successful login action
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid username or password");
                 }
             }
+        });
+
+        add(panel, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(null); // Center the form on the screen
+    }
+
+    private boolean authenticate(String username, char[] password) {
+        boolean isValid = false;
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, username);
+                stmt.setString(2, new String(password));
+                ResultSet rs = stmt.executeQuery();
+                isValid = rs.next();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
+        return isValid;
+    }
+}
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new LoginForm().setVisible(true);
+            }
+        });
     }
 }
